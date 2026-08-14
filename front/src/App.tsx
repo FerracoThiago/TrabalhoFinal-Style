d6d069bcc9c071570a8c2a849c884ba4dfc86a79
MarcosJuniors <marccosjunior9633@gmail.com>
2026-08-13 23:25:16 -0300

    feat: implementa telas de Cadastro, Header e Topbar idênticas ao protótipo

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,15 @@
+import { Header } from './components/Header/Header';
+import { Cadastro } from './pages/Cadastro/Cadastro';
+
 export function App() {
   return (
-      <h1 className="text-3xl font-bold text-pink-50">Hello, World!</h1>
-  )
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main>
+        <Cadastro />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
