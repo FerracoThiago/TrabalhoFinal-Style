f11dcc5b1b3a31dcddc1322ff4f1ee6bee0c6778
Vinícius Leoni <viniciuslg@dcc.ufrj.br>
2026-08-15 14:33:47 -0300

    refactor: remoção da função createUser na controller de usuario a pedido do techlead

--- back/src/controllers/UserController.ts
+++ back/src/controllers/UserController.ts
@@ -7,35 +7,6 @@ import { prisma } from "../config/prisma";
 
 export class UserController {
 
-    public static async createUser(req: Request, res: Response){
-        /*Field Validator middleware (zod)*/ 
-
-        try{
-        const {firstName, lastName, email, phone, birthday, gender, password} = req.body;
-        const {salt,hash} = auth.generatePassword(password);
-        const createdUser = await prisma.user.create({
-            data:{
-                firstName,
-                lastName,
-                email,
-                phone,
-                birthday,    //birthday: new Date(birthday) quando alterar a model
-                gender,
-                hash,
-                salt,
-                notifications: 0
-            }
-            });
-            return res.status(201).json({message: "Usuario criado com sucesso!",id: createdUser.id});
-
-        }
-
-        catch(e: any){
-            return res.status(500).json({message: e.message});
-        }
-
-    }
-
     public static async readUser(req:Request, res: Response){
         /*Auth token middleware */
         /*Identify user by token middleware */
