0bc48fedbc8648f58b1860f3249ccc744381ad94
Vinícius Leoni <viniciuslg@dcc.ufrj.br>
2026-08-15 14:29:51 -0300

    feat: criação da função updateUser na controller

--- back/src/controllers/UserController.ts
+++ back/src/controllers/UserController.ts
@@ -60,4 +60,37 @@ export class UserController {
             return res.status(500).json({message:e.message});
         }
     }
+
+    public static async updateUser(req:Request,res:Response){
+        /*Auth token middleware */
+        /*Identify user by token middleware */
+        /*Field validator middleware */
+        try {
+            const userId = res.locals.user.id;      //via mittleware de identificação do id pelo token
+            const foundUser = await prisma.user.findUnique({where:{id:userId}});
+            if(!foundUser){
+                return res.status(404).json({message:"usuario não encontrado"});
+            }
+            const {firstName,lastName,email,phone, birthday, gender, password, notifications} = req.body;
+            const {salt,hash} = auth.generatePassword(password);
+            const updatedUser = await prisma.user.update({
+                data:{
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    birthday,
+                    gender,
+                    hash,
+                    salt,
+                    notifications
+
+                },where:{id:userId}
+            });
+            return res.status(200).json({message: "informações do usuario atualizadas!", id: userId});
+
+        } catch (e:any) {
+            return res.status(500).json({message:e.message});
+        }
+    }
 }
