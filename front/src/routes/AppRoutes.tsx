5e7ff8c1065b4fc6a15dca8e3b8f4ddd5bd02555
MarcosJuniors <marccosjunior9633@gmail.com>
2026-08-18 22:31:05 -0300

    bugfix: corrige importacao nomeada do Header no AppRoutes

--- front/src/routes/AppRoutes.tsx
+++ front/src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Header } from '../components/Header';
+import Header from '../components/Header';
 import { Cadastro } from '../pages/Cadastro';
 import { Login } from '../pages/Login';
 
