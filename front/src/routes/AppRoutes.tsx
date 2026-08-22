74999d4ddfc5dbc08c9d184cbfb23587a5bc02a5
MarcosJuniors <marccosjunior9633@gmail.com>
2026-08-21 21:39:11 -0300

    feat: adiciona estrutura inicial do cart

--- front/src/routes/AppRoutes.tsx
+++ front/src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 import { Cadastro } from '../pages/Cadastro';
 import { Login } from '../pages/Login';
+import { Cart } from '../pages/Cart';
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ export const AppRoutes: React.FC = () => {
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
     </div>
