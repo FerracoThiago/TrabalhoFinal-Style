1ebc8121dfa35facaec84922e4d90ed9c7d60703
Vinícius Leoni <viniciuslg@dcc.ufrj.br>
2026-08-27 15:07:58 -0300

    feat: criação das rotas da controller de Review

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { photoUpload } from "../config/uploads";
+import { AddressController } from "../controllers/AddressController";
 import { AuthController } from "../controllers/AuthController";
 import { CartController } from "../controllers/CartController";
 import { CouponController } from "../controllers/CouponController";
+import { OrderController } from "../controllers/OrderController";
 import { ProductController } from "../controllers/ProductController";
+import { ReviewController } from "../controllers/ReviewController";
 import { UserController } from "../controllers/UserController";
 import { VariantController } from "../controllers/VariantController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { validateCreateProduct, validateUpdateProduct } from "../middlewares/productMiddleware";
 import { validateCreateVariant, validateRegister, validateUpdateVariant } from "../middlewares/validateMiddleware";
-import { AddressController } from "../controllers/AddressController";
-import { OrderController } from "../controllers/OrderController";
 
 const router = Router();
 
@@ -66,5 +67,13 @@ router.get("/order/:id", OrderController.readOrder);
 router.get("/order", OrderController.readAllOrders);
 router.put("/order/:id", OrderController.updateOrder);
 router.delete("/order/:id", OrderController.deleteOrder);
+
+/*Review Controller */
+router.post("/review",authMiddleware,ReviewController.createReview);
+router.get("/review/:reviewId", ReviewController.readReview);
+router.get("/review",ReviewController.readAllReviews);
+router.get("review/product/:productId", ReviewController.readAllReviewsFromProduct);
+router.put("/review", authMiddleware, ReviewController.updateReview);
+router.delete("/review", authMiddleware, ReviewController.deleteReview);
 
 export default router;
